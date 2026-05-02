2109d077a91ee6a78884ea6dd9c36a32221c13fa
KUSH <grow1june2006@gmail.com>
2026-05-02 22:23:35 +0530

    fix port

--- app.py
+++ app.py
@@ -39,4 +39,6 @@ def all_data():
     tags = " ".join(random.sample(hashtags, 4))
     return f"{cap}\\n\\n{tags}"
 
-app.run(host='0.0.0.0', port=5000)
+import os
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
